ef821fb272b73063c4a51e012fa56ee9c65d323e
Satori <35647148+sasasatori@users.noreply.github.com>
2025-09-07 01:18:27 +0800

    Delete dw_fp_mac directory

--- dw_fp_mac/work/dc/scripts/dc_set_cons.tcl
+++
@@ -1,6 +0,0 @@
-
-set sdc_path "../../src/sdc" 
-
-read_sdc "$sdc_path/${design_name}.sdc"
-
-
--- dw_fp_mac/work/dc/scripts/dc_set_env.tcl
+++
@@ -1,5 +0,0 @@
-set_operating_conditions slow -library slow	
-
-set_wire_load_model -name "smic18_wl10"
-set_wire_load_mode top
-
